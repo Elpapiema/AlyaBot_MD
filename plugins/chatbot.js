03441632d0a7df9021f8a196a470dd7b588d7305
Elpapiema <145789380+Elpapiema@users.noreply.github.com>
2024-09-16 21:26:34 -0600

    Arreglo de bugs (otra vez)

--- plugins/chatbot.js
+++ plugins/chatbot.js
@@ -6,29 +6,60 @@ handler.all = async function (m) {
 let chat = global.db.data.chats[m.chat]
 let name = conn.getName(m.sender)
 if (chat.isBanned) return
-if (m.isBot || m.sender.includes(`bot`) || m.sender.incudes(`bot`)) {
-return true;
+if (m.isBot || m.sender.includes('bot') || m.sender.includes('Bot')) {
+return true; 
 }
 let vn = 'https://qu.ax/Ocxm.mp3'
 let bot = `${pickRandom([`*¡𝑬𝒚! 𝑨𝒒𝒖í 𝒆𝒔𝒕𝒐𝒚. 𝒀𝒐 𝒑𝒖𝒆𝒅𝒐 𝒂𝒚𝒖𝒅𝒂𝒓 👉👈 𝑯𝒆𝒚! 𝑰'𝒎 𝒉𝒆𝒓𝒆. 𝑰 𝒄𝒂𝒏 𝒉𝒆𝒍𝒑 🙌*`, `Aqui estoy | Here I am 😼`, `*Hola Aqui estoy yo puedo ayudar? | Hello, here I am, can I help? 😸*`])}`.trim()
-let txt = `*` 
+let txt = `*¿Tu Nokia es muy lento y necesitas que tu bot esté activo 24/7?* 📱⏳
 
-if (/^infohost|hosting|host|corin|corinplus$/i.test(m.text)) {
+¡Tenemos la solución perfecta para ti! 🎉 Mantén tu bot funcionando sin interrupciones con nuestros servidores, Ofrecemos servidores gratuitos y de pago a precios súper accesibles, al alcance de todos. 💸 
+
+🖥️ *Totalmente compatible con GataBot:* Disfruta al máximo de su potencial en nuestros servidores de alto rendimiento, asegurando una experiencia fluida y de alta calidad. El staff de GataBot y CorinPlus Host se encarga de que disfrutes de todas sus funciones al máximo. 😺✨
+
+🟢 \`\`\`Información del Host\`\`\`
+
+💻 *Página:*
+https://dash.corinplus.com
+
+*🟢 Dashboard:*
+https://dash.corinplus.com
+
+⚙️ *Panel*
+https://panel.corinplus.com
+
+💥 *Comunidad de WhatsApp:*
+https://chat.whatsapp.com/HR3OLhsuZPqCMImzuHcuON
+
+*🟣 Discord:*
+https://discord.com/invite/bjKpRBtkHv
+
+🧡 *Canal de WhatsApp:*
+https://whatsapp.com/channel/0029VakUvreFHWpyWUr4Jr0g
+
+🗣📲 *Contacto:*
+• wa.me/5214531287294
+• wa.me/573147616444
+• https://www.facebook.com/elrebelde21
+
+No esperes más y lleva tu bot al siguiente nivel con nuestro servicio de alojamiento. ¡Es fácil, rápido y económico! 💪🚀` 
+
+if (/^infohost|hosting|corinplus|CorinPlus$/i.test(m.text)) {
  await conn.sendMessage(m.chat, { text: txt,
 contextInfo:{
 forwardedNewsletterMessageInfo: { 
-newsletterJid: '120363310525923571@newsletter', 
+newsletterJid: '120363301598733462@newsletter', 
 serverMessageId: '', 
-newsletterName: 'AlyaBot & RaphtaliaBot' }, 
+newsletterName: 'CorinPlus-Host ☁️' }, 
 forwardingScore: 9999999,
 isForwarded: true, 
 "externalAdReply": {
 "showAdAttribution": true,
 "containsAutoReply": true,
-title: `*`,
-body: `*`,
+title: `🤖 𝐂O𝐑𝐈𝐍𝐏𝐋𝐔𝐒-𝐇𝐎𝐒𝐓 🤖`,
+body: `¡El plus que necesitas!_`,
 "previewType": "PHOTO",
-thumbnailUrl: '*', 
+thumbnailUrl: 'https://telegra.ph/file/551d3d544d7bc607fd337.jpg', 
 sourceUrl: accountsgb}}},
 { quoted: fkontak})
 } 
@@ -56,9 +87,40 @@ conn.reply(m.chat, `*╭┅〘 ⚠️ 𝗢𝗯𝗲𝗱𝗲𝗰𝗲 𝗹𝗮𝘀 
 *╰═┅ৡৢ͜͡✦═╡ 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 ╞═┅ৡৢ͜͡✦═╯*`, fkontak, m)}
 
 if (/^Quiero un bot|como obtengo un bot?|Quiero un bot?|quiero un bot|solicitud|solicitó bot|solicito bot|Necesito un bot|necesito un bot$/i.test(m.text) ) {
-conn.reply(m.chat,  `\`*\`
+conn.reply(m.chat,  `\`⚡¿Quieres un bot para tu grupo?\`
 
-*
+*🐈 Tiene varias opciones. Puedes instalarlo tú mismo siguiendo los pasos de instalación:*
+* #instalarbot
+
+*🧡 Puede hacerte un sub bot mandando el siguiente comando:*
+* #serbot (escanea el QR) 
+* #jadibot --code (Código de 8 dígitos)
+
+*💖 Puedes solicitarlo haciendo una donación voluntaria a través de PayPal o Mercado Pago arg.*
+
+> 🚀 El bot estará activo 24/7 para tu grupo.
+
+\`⚡ ¿Por dónde puedo donar?\`
+> A través de nuestro PayPal o Mercado Pago.
+
+*❇️PayPal:*
+• https://www.paypal.com/paypalme/OficialGD
+
+*❇️Mercado pago:*
+
+*• Alias :* OficialGB
+*• CVU :* 0000003100059201491917
+
+\`⏩ Siguiente paso ⏩\`
+
+> Una vez realizado el pago, puedes enviar un comprobante de envío del dinero (captura de pantalla) para que pueda agregar el bot a tu grupo:
+
+• https://chat.whatsapp.com/FDRfhecUGrCEQswkg8FUYz
+• ${ig}
+• https://www.facebook.com/elrebelde21
+
+\`⚡ ¿El bot estará activo 24/7?\`
+_*Sí, nuestro bot está alojado en un servidor de pago para mantenerlo activo 24/7 (por eso también solicitamos donaciones para mantenerlo en funcionamiento) 💞*_
 
 > *𝙂𝙧𝙖𝙘𝙞𝙖𝙨 𝙥𝙤𝙧 𝙨𝙪𝙨 𝙥𝙧𝙚𝙛𝙚𝙧𝙚𝙣𝙘𝙞𝙖𝙨 𝙚𝙣 ${gt} 🐈💞*`, fkontak, {contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: `Hola ${name} 👋`, body: wm, previewType: 0, thumbnail: gataImg, sourceUrl: accountsgb }}})}
  
@@ -70,7 +132,7 @@ conn.reply(m.chat, `\`✨ ¿𝐐𝐮𝐞́ 𝐞𝐬 𝐮𝐧 𝐁𝐨𝐭 𝐝�
 🍃 *_Para ver el menú de comandos puedes usar:_*
 #menu
 
-🐈 * 🐈`, m)}  
+🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 🐈`, m)}  
 return !0 
 }
 export default handler

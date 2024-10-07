91323832a3a40a789f5ed2d76801682610a158d3
Elpapiema <145789380+Elpapiema@users.noreply.github.com>
2024-10-07 01:32:45 -0600

    Actualizar rollwaifu-harem.js

--- plugins/rollwaifu-harem.js
+++ plugins/rollwaifu-harem.js
@@ -37,7 +37,7 @@ let handler = async (m, { conn }) => {
         });
 
         // Enviar el mensaje con la lista de personajes y la imagen personalizada
-        await conn.sendFile(m.chat, 'https://example.com/imagen.jpg', 'harem.jpg', message, m);
+        await conn.sendFile(m.chat, 'https://qu.ax/uXxWp.jpg', 'harem.jpg', message, m);
     } catch (error) {
         await conn.reply(m.chat, `Error al cargar el harem: ${error.message}`, m);
     }

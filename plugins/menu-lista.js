bc37c6d6f996c65fac72cd0701369e18d0316c08
𝓔𝓶𝓶𝓪 (𝓥𝓲𝓸𝓵𝓮𝓽'𝓼 𝓥𝓮𝓻𝓼𝓲ó𝓷) <145789380+Elpapiema@users.noreply.github.com>
2024-10-11 06:48:07 -0600

    Actualizar menu-lista.js

--- plugins/menu-lista.js
+++ plugins/menu-lista.js
@@ -11,27 +11,10 @@ let handler = async (m, { conn }) => {
         const randomVideoUrl = videoUrls[Math.floor(Math.random() * videoUrls.length)];
 
         // Texto del mensaje del menú
-        const menuMessage = 'Bienvenido al menú principal. Presiona el botón "Más opciones" para ver todas las opciones disponibles.';
+        const menuMessage = 'Bienvenido al menú principal. Usa el comando .allmenu para ver todas las opciones disponibles.';
 
         // Enviar el video aleatorio como archivo de video normal
         await conn.sendFile(m.chat, randomVideoUrl, 'menu.mp4', menuMessage, m);
-
-        // Configuración del botón
-        const buttonMessage = {
-            caption: 'Selecciona una opción:',
-            footer: 'Menú de Opciones',
-            buttons: [
-                {
-                    buttonId: '.allmenu', // Comando que se ejecutará cuando se presione el botón
-                    buttonText: { displayText: 'Más opciones' }, // Texto del botón
-                    type: 1
-                }
-            ],
-            headerType: 5 // Indica que el mensaje tiene un GIF o video
-        };
-
-        // Enviar el mensaje con el botón
-        await conn.sendMessage(m.chat, buttonMessage, { quoted: m });
 
     } catch (error) {
         await conn.reply(m.chat, `Error al cargar el menú: ${error.message}`, m);

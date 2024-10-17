374f2ca7b02a2bd7eded7601b5eca41dcffab821
𝓔𝓶𝓶𝓪 (𝓥𝓲𝓸𝓵𝓮𝓽'𝓼 𝓥𝓮𝓻𝓼𝓲ó𝓷) <145789380+Elpapiema@users.noreply.github.com>
2024-10-16 19:37:01 -0600

    Actualizar menu-lista.js

--- plugins/menu-lista.js
+++ plugins/menu-lista.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
     try {
         // URLs de los videos que se enviarán
         const videoUrls = [
-            'https://qu.ax/CUVel.mp4', // Primer video
+            'https://files.catbox.moe/b5n81s.mp4', // Primer video
             'https://qu.ax/auYbf.mp4', // Segundo video
             'https://qu.ax/ocrFx.mp4'  // Tercer video
         ];

13650ae4c47bc0f0f3db5bd633d7476335f64eab
𝓔𝓶𝓶𝓪 (𝓥𝓲𝓸𝓵𝓮𝓽'𝓼 𝓥𝓮𝓻𝓼𝓲ó𝓷) <145789380+Elpapiema@users.noreply.github.com>
2024-10-19 23:08:02 -0600

    Actualizar descargas-yta.js

--- plugins/descargas-yta.js
+++ plugins/descargas-yta.js
@@ -33,7 +33,7 @@ const size = await yt.audio[q].fileSizeH
 await conn.sendFile(m.chat, dl_url, ttl + '.mp3', null, m, false, { mimetype: 'audio/mp4' })
 } catch {
 try {
-let lolhuman = await fetch(`https://deliriusapi-official.vercel.app/download/ytmp3?&url=${youtubeLink}`)    
+let lolhuman = await fetch(`https://api.eliasar.cloud/api/ytplay?url=${youtubeLink}&apikey=57412xovi13ci97y1sk2yw`)    
 let lolh = await lolhuman.json()
 let n = lolh.result.title || 'error'
 await conn.sendMessage(m.chat, { audio: { url: lolh.result.link }, fileName: `${n}.mp3`, mimetype: 'audio/mp4' }, { quoted: m })  

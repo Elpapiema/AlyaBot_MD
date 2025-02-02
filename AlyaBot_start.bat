38aebc1338481f2773769f9ac3bf625a1ac70fb9
Elpapiema <145789380+Elpapiema@users.noreply.github.com>
2025-02-02 14:53:00 -0600

    Update AlyaBot_start.bat

--- AlyaBot_start.bat
+++ AlyaBot_start.bat
@@ -1,40 +1,29 @@
 @echo off
-REM Verificar si el script se ejecuta como administrador
-net session >nul 2>&1
-if %errorlevel% == 0 (
-    echo Ejecutando con permisos de administrador...
-) else (
-    echo Solicitud de permisos de administrador...
-    REM Solicitar elevación de permisos
-    powershell -Command "Start-Process '%~f0' -Verb runAs"
-    exit /b
-)
-
 REM Verificar si Node.js está instalado
 where node >nul 2>nul
 if %errorlevel% neq 0 (
-    echo Node.js no está instalado. Ejecutando instalador...
+    echo Node.js no esta instalado. Ejecutando instalador...
     start /wait node-installer.exe
 ) else (
-    echo Node.js ya está instalado.
+    echo Node.js ya esta instalado.
 )
 
 REM Verificar si Git está instalado
 where git >nul 2>nul
 if %errorlevel% neq 0 (
-    echo Git no está instalado. Ejecutando instalador...
+    echo Git no esta instalado. Ejecutando instalador...
     start /wait git-installer.exe
 ) else (
-    echo Git ya está instalado.
+    echo Git ya esta instalado.
 )
 
-REM Verificar si ImageMagick está instalado
+REM Verificar si ImageMagick esta instalado
 where convert >nul 2>nul
 if %errorlevel% neq 0 (
-    echo ImageMagick no está instalado. Ejecutando instalador...
+    echo ImageMagick no esta instalado. Ejecutando instalador...
     start /wait imagemagick-installer.exe
 ) else (
-    echo ImageMagick ya está instalado.
+    echo ImageMagick ya esta instalado.
 )
 
 REM Ejecutar Git pull

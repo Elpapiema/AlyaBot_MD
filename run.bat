7a56dde65e1c149fe090411dc0fe4a43be4e4954
𝓔𝓶𝓶𝓪 (𝓥𝓲𝓸𝓵𝓮𝓽'𝓼 𝓥𝓮𝓻𝓼𝓲ó𝓷) <145789380+Elpapiema@users.noreply.github.com>
2025-02-16 16:37:50 -0600

    windows

--- run.bat
+++ run.bat
@@ -1,15 +1,15 @@
 @echo off
 REM Ejecutar el archivo setup.bat
-echo [INFO] Verificando si los programas de ejecucion estan instalados...
+echo [Console @ AlyaBot] Verificando si los programas de ejecucion estan instalados...
 call setup.bat
 
 REM Verificar si setup.bat terminó correctamente
 if %errorlevel% neq 0 (
-    echo [ADVERTENCIA] setup.bat termino con errores, pero se continuara con la ejecución.
+    echo [Console @ AlyaBot] setup.bat termino con errores, pero se continuara con la ejecución.
 )
 
 REM Ejecutar node index.js
-echo [INFO] Iniciando AlyaBot...
+echo [Console @ AlyaBot] Iniciando AlyaBot...
 node index.js
 if %errorlevel% neq 0 (
     echo [ERROR] Ocurrio un error. Verifica la instalacion.
--- setup.bat
+++ setup.bat
@@ -2,39 +2,39 @@
 REM Verificar si Node.js está instalado
 where node >nul 2>nul
 if %errorlevel% neq 0 (
-    echo [INFO] Node.js no está instalado. Ejecutando instalador...
+    echo [Console @ AlyaBot] Node.js no esta instalado. Ejecutando instalador...
     start /wait node-installer.msi
 ) else (
-    echo [INFO] Node.js ya esta instalado.
+    echo [Console @ AlyaBot] Node.js ya esta instalado.
 )
 
 REM Verificar si Git está instalado
 where git >nul 2>nul
 if %errorlevel% neq 0 (
-    echo [INFO] Git no está instalado. Ejecutando instalador...
+    echo [Console @ AlyaBot] Git no esta instalado. Ejecutando instalador...
     start /wait git-installer.exe
 ) else (
-    echo [INFO] Git ya esta instalado.
+    echo [Console @ AlyaBot] Git ya esta instalado.
 )
 
 REM Verificar si ImageMagick está instalado
 where convert >nul 2>nul
 if %errorlevel% neq 0 (
-    echo [INFO] ImageMagick no está instalado. Ejecutando instalador...
+    echo [Console @ AlyaBot] ImageMagick no esta instalado. Ejecutando instalador...
     start /wait imagemagick-installer.exe
 ) else (
-    echo [INFO] ImageMagick ya esta instalado.
+    echo [Console @ AlyaBot] ImageMagick ya esta instalado.
 )
 
 REM Ejecutar Git pull
-echo [INFO] Verificando Actualizaciones...
+echo [Console @ AlyaBot] Verificando Actualizaciones...
 git pull
 
 REM Ejecutar npm install (ignorar crasheo)
-echo [INFO] Instalando Dependencias...
+echo [Console @ AlyaBot] Instalando Dependencias...
 npm install
 if %errorlevel% neq 0 (
-    echo [ADVERTENCIA] Se detectó un problema después de npm install, pero se ignorará para continuar.
+    echo [Console @ AlyaBot] Se detectó un problema después de npm install, pero se ignorará para continuar.
 )
 
 REM Ejecutar node index.js

e8705cd4cb172d5d78f9bcc547ca87dba4ca02cb
𝓔𝓶𝓶𝓪 (𝓥𝓲𝓸𝓵𝓮𝓽'𝓼 𝓥𝓮𝓻𝓼𝓲ó𝓷) <145789380+Elpapiema@users.noreply.github.com>
2025-04-14 21:20:29 -0600

    Corregir mensaje de error y actualizar respuesta de sub bots en sub-bots_bots.js

--- plugins/sub-bots_bots.js
+++ plugins/sub-bots_bots.js
@@ -3,7 +3,7 @@ import ws from 'ws';
 async function handler(m, { conn: _envio, usedPrefix }) {
 const msgTxt = await ("✅ Prestar el bot para unirlo a grupos")
 const msgTxt2 = await ("Tiempo activo")
-const msgTxt3 = await ("*No hay Sub bots conectado, verifique mas tardes.*")
+const msgTxt3 = await ("*No hay Sub bots conectado, verifique mas tarde.*")
 const msgTxt4 = await ("Username")
 const users = [...new Set([...global.conns.filter((conn) => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map((conn) => conn)])];
 
@@ -38,10 +38,9 @@ const prestarStatus = botConfig.privacy ? '' : (botConfig.prestar ? msgTxt : '')
 return `👉🏻 ${botNumber} (${v.user.name || '-'})\n*🔰 ${msgTxt2} :* ${v.uptime ? convertirMsADiasHorasMinutosSegundos(Date.now() - v.uptime) : "Desconocido"}\n${prestarStatus}`}).join('\n\n');
 const replyMessage = message.length === 0 ? msgTxt3 : message;
 const totalUsers = users.length;
-const responseMessage = await (`*🤖 Aquí tienes la lista de algunos sub bots (jadibot/serbot) 🤖️*\n\n*👉🏻 Puedes contactarlos para ver si se unen a tu grupo*\n\n*Te pedimos de favor que:*\n*1.- Seas amable ✅*\n*2.- No insistas ni discutas ✅*\n\n*✳ ️Si le aparece el siguiente texto en blanco es que no hay ningún sub bot disponible en este momento inténtelo mas tarde*\n\n*_⚠ NOTA: ️ELLOS SON PERSONAS QUE NO CONOCEMOS.. POR LO QUE EL EQUIPO DE LOLIBOT NO SE HACE RESPONSABLE DE LO QUE PUEDA OCURRIR AHI.._*\n\n*🤖 SUBBOTS CONECTADO :* `) + `${totalUsers || '0'}\n\n${replyMessage.trim()}`.trim();
-await _envio.sendMessage(m.chat, {text: responseMessage, contextInfo: { mentionedJid: _envio.parseMention(responseMessage), externalAdReply: { mediaUrl: null, mediaType: 1, description: null, title: wm, body: '𝐒𝐮𝐩𝐞𝐫 𝐁𝐨𝐭 𝐃𝐞 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩', previewType: 0, thumbnail: null, sourceUrl: null}}}, { quoted: m });
+const responseMessage = await (`*🤖 Aquí tienes la lista de algunos sub bots  🤖️*\n\n*👉🏻 Puedes contactarlos para ver si se unen a tu grupo*\n\n*Te pedimos de favor que:*\n*1.- Seas amable ✅*\n*2.- No insistas ni discutas ✅*\n\n*✳ ️Si le aparece el siguiente texto en blanco es que no hay ningún sub bot disponible en este momento inténtelo mas tarde*\n\n*_⚠ NOTA: Estas sesiones son de personas cuya identidad es ajena al Staff de AlyaBot Por lo que no nos hacemos responsables de lo que pase ahi .._*\n\n*🤖 SUBBOTS CONECTADO :* `) + `${totalUsers || '0'}\n\n${replyMessage.trim()}`.trim();
+await _envio.sendMessage(m.chat, {text: responseMessage, contextInfo: { mentionedJid: _envio.parseMention(responseMessage)/*, externalAdReply: { mediaUrl: null, mediaType: 1, description: null, title: wm, body: '𝐒𝐮𝐩𝐞𝐫 𝐁𝐨𝐭 𝐃𝐞 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩', previewType: 0, thumbnail: null, sourceUrl: null}*/}}, { quoted: m });
 }
 handler.command = handler.help = ['listjadibot', 'bots'];
 handler.tags = ['jadibot'];
-handler.register = true;
 export default handler;
